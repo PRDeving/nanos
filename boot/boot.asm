785940b14350c845a0de3b72cb893f6349c8a84b
prdeving <pablo.deving@gmail.com>
2016-09-28 16:05:35 +0200

    print16 asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -10,37 +10,45 @@ KERNEL_OFFSET equ 0x1000
 	mov bx, MSG_BOOTSTRAP_INIT
 	call print16
 
-	call loadKernel
-
-	call switchProtected32
+	; call loadKernel
+  ;
+	; call switchProtected32
 
 	jmp $
 
 [bits 16]
-; print16:
-	
-
-loadKernel:
-	mov bx, MSG_KERNEL_INIT
-	call print16
-
-	mov bx, KERNEL_OFFSET
-	mov dh, 15 ;15 sectors
-	mov dl, [BOOT_DRIVE]
-	call loadFromDisk
+print16:
+	mov ah, 0x0e
+	mov al, [bx]
+	int 0x10
+	add bx, 1
+	cmp al, 0
+	jne print16
+	mov al, 10
+	int 0x10
 	ret
 
-[bits 32]
-switchProtected32:
-	mov ebx, MSG_KERNEL_LOAD
-	call print32
-	call KERNEL_OFFSET
+; loadKernel:
+; 	mov bx, MSG_KERNEL_INIT
+; 	call print16
+;
+; 	mov bx, KERNEL_OFFSET
+; 	mov dh, 15 ;15 sectors
+; 	mov dl, [BOOT_DRIVE]
+; 	call loadFromDisk
+; 	ret
+;
+; [bits 32]
+; switchProtected32:
+; 	mov ebx, MSG_KERNEL_LOAD
+; 	call print32
+; 	call KERNEL_OFFSET
 
 
 BOOT_DRIVE db 0
-MSG_BOOTSTRAP_INIT db "Starting 16bit bootstrap", 0
-MSG_KERNEL_INIT db "Starting kernel loading", 0
-MSG_KERNEL_LOAD db "Launching kernel", 0
+MSG_BOOTSTRAP_INIT: db "Starting 16bit bootstrap", 0
+MSG_KERNEL_INIT: db "Starting kernel loading", 0
+MSG_KERNEL_LOAD: db "Launching kernel", 0
 
 ; Set as bootable
 times 510-($-$$) db 0
