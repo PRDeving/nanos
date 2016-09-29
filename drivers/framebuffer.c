affcdbb35e7b6267c1094932809ab89168e5ee8d
prdeving <pablo.deving@gmail.com>
2016-09-29 17:45:23 +0200

    framebuffer driver over io.asm methods

--- drivers/framebuffer.c
+++ drivers/framebuffer.c
@@ -1,16 +1,42 @@
-#include <asm/io.h>
+#include <drivers/framebuffer.h>
 
 #define CPORT 0x3D4
 #define DPORT 0x3D5
 #define HBYTE 14
 #define LBYTE 15
 
-void move_cursor(unsigned short pos) {
+unsigned int FB_CURSOR_X = 0;
+unsigned int FB_CURSOR_Y = 0;
+
+unsigned short pos_to_cursor(int x, int y) {
+  return (y * 80) + x;
+}
+
+void move_cursor_to (int x, int y) {
+  unsigned short pos = pos_to_cursor(x, y);
   outb(CPORT, HBYTE);
   outb(DPORT, ((pos >> 8) & 0x00FF));
   outb(CPORT, LBYTE);
   outb(DPORT, pos & 0x00FF);
+  FB_CURSOR_X = x;
+  FB_CURSOR_Y = y;
 }
 
-/* unsigned short get_cursor(void) { */
-/* } */
+void cursor_next(void) {
+  FB_CURSOR_X++;
+  if (FB_CURSOR_X > 79) {
+    FB_CURSOR_Y++;
+    FB_CURSOR_X = 0;
+  }
+  move_cursor_to(FB_CURSOR_X, FB_CURSOR_Y);
+}
+
+void cursor_next_line(void) {
+  FB_CURSOR_Y++;
+  FB_CURSOR_X = 0;
+  move_cursor_to(FB_CURSOR_X, FB_CURSOR_Y);
+}
+
+unsigned short get_cursor(void) {
+  return ((FB_CURSOR_Y * 80) + FB_CURSOR_X) * 2;
+}
--- include/drivers/framebuffer.h
+++ include/drivers/framebuffer.h
@@ -1,7 +1,14 @@
 #ifndef H_FRAMEBUFFER_C
 #define H_FRAMEBUFFER_C
-#include <asm/io.h>
 
-void move_cursor(unsigned short);
+#include <asm/io.h>
+#define FB_DEFAULT 0x0f
+#define FB_GREEN_IN_BLACK 0x02
+
+void move_cursor_to(int, int);
+void cursor_next(void);
+void cursor_next_line(void);
+unsigned short get_cursor(void);
+unsigned short pos_to_cursor(int, int);
 
 #endif
